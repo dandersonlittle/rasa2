5a5250b1943fbf3edb586a4fa8a90bddd027cf64
Daniel Anderson-Little <bigdog@Daniels-Mac-mini.local>
2024-02-27 20:13:21 -0600

    Initial Bitcoin price API

--- actions/actions.py
+++ actions/actions.py
@@ -25,3 +25,34 @@
 #         dispatcher.utter_message(text="Hello World!")
 #
 #         return []
+
+
+# Import necessary modules
+from rasa_sdk import Action
+from rasa_sdk.events import SlotSet
+import requests
+
+class ActionGetBitcoinPrice(Action):
+    def name(self):
+        # Name of the custom action
+        return "action_get_bitcoin_price"
+
+    def run(self, dispatcher, tracker, domain):
+        # URL for CoinGecko API to get Bitcoin price
+        url = "https://api.coingecko.com/api/v3/simple/price?ids=bitcoin&vs_currencies=usd"
+
+        # Make a GET request to the CoinGecko API
+        response = requests.get(url)
+        data = response.json()
+
+        # Extract the price of Bitcoin from the API response
+        bitcoin_price = data["bitcoin"]["usd"]
+
+        # Create the response message
+        message = f"The current price of Bitcoin is ${bitcoin_price} USD."
+
+        # Send the message back to the user
+        dispatcher.utter_message(text=message)
+
+        # Optionally, you can set a slot with the Bitcoin price if you want to use it later
+        return [SlotSet("bitcoin_price", bitcoin_price)]
